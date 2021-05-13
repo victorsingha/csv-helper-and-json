35a6109b7d46802f33f7975c2d45ed7cf3c7b93b
victor-singha <vicunite2@gmail.com>
2021-05-13 13:15:46 +0530

    initial commit

---
+++ CSVHelperAndJSON/CSVHandler.cs
@@ -0,0 +1,46 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVHelperAndJSON
+{
+    class CSVHandler
+    {
+        public static void ImplementCSVDataHandling()
+        {
+            string importFilePath = @"C:\Users\vicun\source\repos\CSVHelperAndJSON\CSVHelperAndJSON\Utility\Addresses.csv";
+            string exportFilePath = @"C:\Users\vicun\source\repos\CSVHelperAndJSON\CSVHelperAndJSON\Utility\export.csv";
+
+            //reading csv file
+            using(var reader = new StreamReader(importFilePath))
+            using(var csv = new CsvReader(reader,CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read Data Successfully from address.csv");
+                foreach (AddressData addressData in records)
+                {
+                    Console.Write("\t" + addressData.firstname);
+                    Console.Write("\t" + addressData.lastname);
+                    Console.Write("\t" + addressData.address);
+                    Console.Write("\t" + addressData.city);
+                    Console.Write("\t" + addressData.state);
+                    Console.Write("\t" + addressData.code);
+
+                    Console.WriteLine("**************Reading from csv file and write to csv file");
+
+                    //writing csv file               
+                    using (var writer = new StreamWriter(exportFilePath))
+                    using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                    {
+                        csvExport.WriteRecords(records);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSVHelperAndJSON/ReadCSVWriteCSV.cs
@@ -0,0 +1,45 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVHelperAndJSON
+{
+    class ReadCSVWriteCSV
+    {
+        public static void ImplementCSVToJSON()
+        {
+            string importFilePath = @"C:\Users\vicun\source\repos\CSVHelperAndJSON\CSVHelperAndJSON\Utility\Addresses.csv";
+            string exportFilePath = @"C:\Users\vicun\source\repos\CSVHelperAndJSON\CSVHelperAndJSON\Utility\export.csv";
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read Data Successfully from address.csv");
+                foreach (AddressData addressData in records)
+                {
+                    Console.Write("\t" + addressData.firstname);
+                    Console.Write("\t" + addressData.lastname);
+                    Console.Write("\t" + addressData.address);
+                    Console.Write("\t" + addressData.city);
+                    Console.Write("\t" + addressData.state);
+                    Console.Write("\t" + addressData.code);
+                }
+                Console.WriteLine("**************Reading from csv file and write to csv file");
+                
+                //writing json file
+                JsonSerializer serializer = new JsonSerializer();
+                using (StreamWriter sw = new StreamWriter(exportFilePath))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, records);
+                }
+            }
+        }
+    }
+}
